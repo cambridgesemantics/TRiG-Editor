0626a3430f48acd006d6010a1e413dd2dac4b8e8
Aron Lurie <aron@cambridgesemantics.com>
2014-08-16 22:44:42 -0400

    Adding copyright message to top of all Java files.

--- TriGEditor/src/trigeditor/editors/XMLPartitionScanner.java
+++ TriGEditor/src/trigeditor/editors/XMLPartitionScanner.java
@@ -1,3 +1,14 @@
+/***********************************************************************
+ * Copyright (c) 2014 Cambridge Semantics Incorporated.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Cambridge Semantics Incorporated - initial API and implementation
+ ***********************************************************************/
+
 package trigeditor.editors;
 
 import org.eclipse.jface.text.TextAttribute;
